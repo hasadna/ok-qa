b801a426828cf879741cb4ba110558f4920144ca
Benny Daon <bennydaon@gmail.com>
2013-09-09 11:21:27 +0200

    Fixing edit button, #247

--- qa/urls.py
+++ qa/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('qa.views',
     url(r'^(?P<entity_slug>[-\w]+)/qna/tags/(?P<tags>.+)/$', 'local_home', name='show_tags'),
 
     url(r'^qna/post_q/$', 'post_question', name='post_question'),
-    url(r'^/qna/(?P<slug>[-\w]+)/edit$', 'post_question', name='edit_question'),
+    url(r'^qna/(?P<slug>[-\w]+)/edit$', 'post_question', name='edit_question'),
     url(r'^(?P<entity_slug>[-\w]+)/qna/(?P<slug>[-\w]+)/$',
         QuestionDetail.as_view(),
         name='question_detail'
