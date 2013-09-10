40a17a86a4da3f2a5875edd145183631c0c8b11a
tbrisker <tbrisker@gmail.com>
2013-09-10 12:22:50 +0300

    fix user urls, add number of council seats to stats

--- oshot/settings/base.py
+++ oshot/settings/base.py
@@ -181,7 +181,7 @@ LOGGING = {
 
 ACCOUNT_ACTIVATION_DAYS = 7 # One-week activation window; you may, of course, use a different value.
 LOGIN_REDIRECT_URL = '/'
-LOGIN_URL = '/login/'
+LOGIN_URL = '/u/login/'
 
 DEFAULT_FROM_EMAIL = 'localshot@hasadna.org.il'
 
