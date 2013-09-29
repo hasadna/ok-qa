3dfac1f9592cb0d9673c35311e50315f00a7e0ec
tbrisker <tbrisker@gmail.com>
2013-09-29 20:55:03 +0300

    allow candidates to edit their lists, and superusers to edit lists in all localities

--- polyorg/urls.py
+++ polyorg/urls.py
@@ -3,7 +3,9 @@ from views import *
 
 urlpatterns = patterns('polyorg.views',
         url(r'list/$', 'candidatelists_list', name='candidate-list-list'),
+        url(r'list/entity/(?P<entity_id>[-\w]+)$', 'candidatelists_list', name='candidate-list-list'),
         url(r'list/create/$', 'candidatelist_edit', name='candidate-list-create'),
+        url(r'list/create/(?P<entity_id>[-\w]+)$', 'candidatelist_edit', name='candidate-list-create'),
         url(r'list/(?P<candidatelist_id>[-\w]+)$', 'candidates_list', name='candidate-list'),
         url(r'list/(?P<candidatelist_id>[-\w]+)/create/$', 'candidate_create', name='candidate-create'),
         url(r'list/(?P<candidatelist_id>[-\w]+)/edit/$', 'candidatelist_edit', name='candidatelist-edit'),
