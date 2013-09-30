8a47a20ade63ad3625ca048a867ef6b12767aae4
tbrisker <tbrisker@gmail.com>
2013-09-30 21:45:51 +0300

    create avatar when updating avatar url in user admin fix #294

--- user/models.py
+++ user/models.py
@@ -15,6 +15,7 @@ from actstream import follow
 from actstream.models import Follow
 # Project's apps
 from entities.models import Entity
+from user.utils import create_avatar
 
 NOTIFICATION_PERIOD_CHOICES = (
     (u'N', _('No Email')),
@@ -84,6 +85,12 @@ class Profile(models.Model):
     on_site = CurrentSiteManager()
 
     objects = ProfileManager()
+
+    def save(self, **kwargs):
+        if self.avatar_uri:
+            create_avatar(self.user, self.avatar_uri)
+        return super(Profile, self).save(**kwargs)
+
     def avatar_url(self, size=40):
         if self.avatar_uri:
             return self.avatar_uri
