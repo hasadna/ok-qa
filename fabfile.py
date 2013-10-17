08e6fc9dc82daacc71c7eebaa0166c3106886e85
Benny Daon <bennydaon@gmail.com>
2013-10-18 01:27:41 +0300

    now supporting `fab deploy:flatblocks=y`
    Usuefull for loading flatblocks fixtures

--- fabfile.py
+++ fabfile.py
@@ -5,13 +5,15 @@ from fabric.contrib.console import confirm
 env.hosts = ['localshot.org.il']
 env.user = 'oshot'
 
-def deploy(branch='master'):
+def deploy(branch='master',flatblocks="no"):
     local('git push origin ' + branch)
     with cd('~oshot/src/oshot'):
         run('git pull origin ' + branch)
         with prefix('. ENV/bin/activate'):
             run('pip install -r requirements.txt')
             run('python manage.py test')
+            if flatblocks[0] == "y":
+                run('honcho run python manage.py loaddata < fixtures/flatblock.json')
             run('honcho run python manage.py syncdb --no-initial-data')
             run('honcho run python manage.py migrate --no-initial-data')
             run('honcho run python manage.py collectstatic --noinput')
