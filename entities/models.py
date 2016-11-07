8c80829b656982c28736a1ed2a3bfe2361ab3ab7
Benny Daon <bennydaon@gmail.com>
2016-11-07 22:59:55 +0200

    getting the models from open-shot

---
+++ entities/models.py
@@ -0,0 +1,258 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from uuidfield import UUIDField
+from autoslug import AutoSlugField
+from slugify import slugify
+
+class ClassMethodMixin(object):
+    """A mixin for commonly used classmethods on models."""
+
+    @classmethod
+    def get_class_name(cls):
+        value = cls.__name__.lower()
+        return value
+
+
+class TimeStampedModel(models.Model):
+    """A mixin to add timestamps to models that inherit it."""
+
+    created_on = models.DateTimeField(
+        _('Created on'),
+        db_index=True,
+        auto_now_add=True,
+        editable=False
+    )
+    last_modified = models.DateTimeField(
+        _('Last modified'),
+        db_index=True,
+        auto_now=True,
+        editable=False
+    )
+
+    class Meta:
+        abstract = True
+
+
+class UUIDModel(models.Model):
+    """A mixin to add UUIDs to models that inherit it."""
+
+    uuid = UUIDField(
+        db_index=True,
+        auto=True
+    )
+
+    class Meta:
+        abstract = True
+
+
+class DomainManager(models.Manager):
+    """Exposes the related_map method for more efficient bulk select queries."""
+
+    def related_map(self):
+        return self.select_related().prefetch_related('divisions')
+
+
+class Domain(TimeStampedModel, ClassMethodMixin):
+    """Domain is the base context for our relational model of entities.
+
+    Entities always belong to a domain, and are structured via Divisions.
+
+    The ability to have multiple domains allows an Open Budget instance to
+    support adjacent or even unrelated data sets. See the docs for more info.
+    """
+
+    MEASUREMENT_SYSTEMS = (
+        ('metric', _('Metric')),
+        ('imperial', _('Imperial'))
+    )
+    GROUND_SURFACE_UNITS = (
+        ('default', _('Default')),
+        ('dunams', _('Dunams'))
+    )
+    CURRENCIES = (
+        ('usd', _('&#36;')),
+        ('ils', _('&#8362;'))
+    )
+
+    objects = DomainManager()
+
+    name = models.CharField(
+        _('Name'),
+        db_index=True,
+        max_length=255,
+        unique=True,
+        help_text=_('The name of this domain.')
+    )
+    measurement_system = models.CharField(
+        _('Measurement System'),
+        max_length=8,
+        choices=MEASUREMENT_SYSTEMS,
+        default=MEASUREMENT_SYSTEMS[0][0],
+        help_text=_('The applicable measurement unit for this domain.')
+    )
+    ground_surface_unit = models.CharField(
+        _('Ground Surface Unit'),
+        max_length=25,
+        choices=GROUND_SURFACE_UNITS,
+        default=GROUND_SURFACE_UNITS[0][0],
+        help_text=_('Rarely to be touched, this field is for countries, like '
+                    'Israel, that use special units for measuring ground.')
+    )
+    currency = models.CharField(
+        _('Currency'),
+        max_length=3,
+        choices=CURRENCIES,
+        default=CURRENCIES[0][0],
+        help_text=_('The currency used for budgeting in this domain.')
+    )
+
+    @property
+    def entities(self):
+        value = Entity.objects.related_map().filter(division__domain=self)
+        return value
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = _('domain')
+        verbose_name_plural = _('domains')
+
+    def __unicode__(self):
+        return self.name
+
+
+class DivisionManager(models.Manager):
+    """Exposes the related_map method for more efficient bulk select queries."""
+
+    def related_map(self):
+        return self.select_related().prefetch_related('entities')
+
+
+class Division(TimeStampedModel, ClassMethodMixin):
+    """Division divides the domain into logical groupings to model structure."""
+
+    objects = DivisionManager()
+
+    domain = models.ForeignKey(
+        Domain,
+        related_name='divisions',
+        help_text=_('The domain that this division belongs to.')
+    )
+    index = models.PositiveSmallIntegerField(
+        _('Index'),
+        db_index=True,
+        default=100,
+        help_text=_('Model the domain structure by positioning this division '
+                    'relative to others. 0 is the highest level. Divisions of '
+                    'equivalent level  should have an equal value.')
+    )
+    name = models.CharField(
+        _('Name'),
+        db_index=True,
+        max_length=255,
+        help_text=_('The name of this division. Divisions under the same Domain'
+                    ' must be named uniquely.')
+    )
+    budgeting = models.BooleanField(
+        _('Budgeting'),
+        db_index=True,
+        default=False,
+        help_text=_('Indicates whether entities that belong to this division '
+                    'are budgeting entities.')
+    )
+
+    @property
+    def entity_count(self):
+        return Entity.objects.related_map().filter(division=self).count()
+
+    class Meta:
+        ordering = ['index', 'name']
+        verbose_name = _('division')
+        verbose_name_plural = _('divisions')
+        unique_together = (
+            ('name', 'domain'),
+        )
+
+    def __unicode__(self):
+        return self.name
+
+
+class EntityManager(models.Manager):
+    """Exposes the related_map method for more efficient bulk select queries."""
+
+    def related_map_min(self):
+        return self.select_related()
+
+    def related_map(self):
+        return self.select_related().prefetch_related('sheets', 'parent')
+
+
+class Entity(TimeStampedModel, ClassMethodMixin):
+    """Entity describes the actual units in our organizational structure."""
+
+    objects = EntityManager()
+
+    division = models.ForeignKey(
+        Division,
+        related_name='entities',
+        help_text=_('The division that this entity belongs to.')
+    )
+    name = models.CharField(
+        _('Name'),
+        db_index=True,
+        max_length=255,
+        help_text=_('The name of this entity.')
+    )
+    description = models.TextField(
+        _('Description'),
+        blank=True,
+        help_text=_('A short text for this entity, useful as an overview.')
+    )
+    code = models.CharField(
+        _('Code'),
+        max_length=25,
+        blank=True,
+        help_text=_('An identifying code for this entity.')
+    )
+    parent = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        related_name='children'
+    )
+    slug = AutoSlugField(
+        db_index=True,
+        populate_from='name',
+        unique=True,
+        slugify = slugify
+    )
+
+    @property
+    def ultimate_parent(self):
+        """Returns the ultimate parent of this object. If none, returns self."""
+        parent = self.parent
+        if parent:
+            return parent.ultimate_parent
+        else:
+            return self
+
+    @property
+    def siblings(self):
+        """Returns all other entities in the same division."""
+
+        return Entity.objects.related_map().filter(division=self.division).\
+            exclude(id=self.id)
+
+    class Meta:
+        ordering = ('division__domain', 'division__index', 'name')
+        verbose_name = _('entity')
+        verbose_name_plural = _('entities')
+        unique_together = (
+            ('name', 'parent', 'division'),
+        )
+
+    def __unicode__(self):
+        return self.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return 'entity_detail', [self.slug]
---
+++ qa/models.py
@@ -0,0 +1,153 @@
+from django.db import models, transaction
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
+from django.core.urlresolvers import reverse
+from django.core.validators import MaxLengthValidator
+from taggit.models import TaggedItemBase
+from django.contrib.contenttypes.models import ContentType
+
+from slugify import slugify as unislugify
+from taggit.managers import TaggableManager
+from entities.models import Entity
+from actstream.models import Follow
+
+
+MAX_LENGTH_Q_SUBJECT = 140
+MAX_LENGTH_Q_CONTENT = 1000
+MAX_LENGTH_A_SUBJECT = 80
+MAX_LENGTH_A_CONTENT = 1000
+
+entity_home_key = lambda entity_id: "entity_home_%s" % entity_id
+
+class BaseModel(models.Model):
+    ''' just a common time base for the models
+    '''
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+    def delete(self, commit=True):
+        self.is_deleted = True
+        if commit:
+            self.save()
+        content_type = ContentType.objects.get_for_model(self)
+        Follow.objects.filter(content_type=content_type, object_id=self.id).delete()
+
+class TaggedQuestion(TaggedItemBase):
+    content_object = models.ForeignKey("Question")
+    # objects = models.Manager()
+
+def can_vote(entity, user):
+    ''' returns whether a secific user can upvote/downvote a question in the
+        entity '''
+    return user.is_authenticated() and user.profile.is_member_of(entity)
+
+Entity.add_to_class('can_vote', can_vote)
+
+class Question(BaseModel):
+
+    # TODO: rename to just `slug`
+    unislug = models.CharField(
+        _('unicode slug'),
+        max_length=MAX_LENGTH_Q_SUBJECT,
+        null=True,
+        blank=True,
+        editable=False
+    )
+    author = models.ForeignKey(User, related_name="questions", verbose_name=_("author"))
+    subject = models.CharField(_("question"), max_length=MAX_LENGTH_Q_SUBJECT)
+    content = models.TextField(_("details"), validators=[MaxLengthValidator(MAX_LENGTH_Q_CONTENT)],
+       help_text=_("Please enter your content in no more than %s letters") % MAX_LENGTH_Q_CONTENT,
+       blank = True, default = '')
+    rating = models.IntegerField(_("rating"), default=1)
+    flags_count = models.IntegerField(_("flags counter"), default=0)
+    tags = TaggableManager(through=TaggedQuestion, blank=True)
+    # for easy access to current site questions
+    objects = models.Manager()
+    entity = models.ForeignKey(Entity, null=True, related_name="questions", verbose_name=_("entity"))
+
+    class Meta:
+        unique_together = ('unislug','entity')
+
+    def __unicode__(self):
+        return self.subject
+
+    def can_answer(self, user):
+        ''' Can a given user answer self? '''
+        if user.is_authenticated():
+            return user.profile.can_answer(self.entity)
+        else:
+            return False
+
+    def get_absolute_url(self):
+        return reverse('question_detail', kwargs=dict(
+                            entity_id=self.entity.id,
+                            slug=self.unislug,
+                            )
+                      )
+
+    def save(self, **kwargs):
+        # make a unicode slug from the subject
+        self.unislug = unislugify(self.subject)
+        return super(Question, self).save(**kwargs)
+
+    #TODO: understand https://docs.djangoproject.com/en/1.10/releases/1.3/#transaction-management
+    # @transaction.commit_on_success
+    def flagged(self):
+        self.flags_count += 1
+        self.save()
+        return self.flags_count
+
+    def can_user_delete(self, user):
+        ''' returns whether a secific user can delete the question '''
+        if self.author == user:
+            return True
+        if user.is_authenticated() and\
+           user.profile.is_editor(self.entity):
+            return True
+        return False
+
+    def can_vote(self, user):
+        ''' returns whether a secific user can upvote/downvote the question,
+            or neither '''
+        if self.entity.can_vote(user):
+            if user.upvotes.filter(question=self).exists():
+                return 'down'
+            else:
+                return 'up'
+        return None
+
+class Answer(BaseModel):
+    author = models.ForeignKey(User, related_name="answers", verbose_name=_("author"))
+    content = models.TextField(_("content"), validators=[MaxLengthValidator(MAX_LENGTH_A_CONTENT)],
+        help_text=_("Please enter an answer in no more than %s letters") % MAX_LENGTH_A_CONTENT)
+    rating = models.IntegerField(_("rating"), default=0)
+    question = models.ForeignKey(Question, related_name="answers", verbose_name=_("question"))
+    # for easy access to current site answers
+    objects = models.Manager()
+
+    def __unicode__(self):
+        return u"%s: %s" % (self.author, self.content[:30])
+
+    def get_absolute_url(self):
+        return '%(url)s?answer=%(id)s#answer-%(id)s' % {'url': self.question.get_absolute_url(), 'id': self.id}
+
+    @property
+    def entity(self):
+        return self.question.entity
+
+class QuestionUpvote(BaseModel):
+    question = models.ForeignKey(Question, related_name="upvotes")
+    user = models.ForeignKey(User, related_name="upvotes")
+
+class QuestionFlag(BaseModel):
+    question = models.ForeignKey(Question, related_name="flags")
+    reporter = models.ForeignKey(User, related_name="flags")
+
+#import signals
+
+# Create your models here.
